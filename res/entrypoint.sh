9f621ed9ddf69677427539199b8b927b9644d4ee
Placeholder6 <86405576+Placeholder6@users.noreply.github.com>
2022-04-09 22:31:49 +0600

    Update entrypoint.sh

--- res/entrypoint.sh
+++ res/entrypoint.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 set -e
 
@@ -6,7 +5,7 @@ cleanup () {
     kill -s SIGTERM $!
     exit 0
 }
-trap cleanup INT TERM
+trap cleanup SIGINT SIGTERM
 
 VNC_IP=$(hostname -i)
 
@@ -22,10 +21,15 @@ echo "$VNC_PW" | vncpasswd -f >> "$PASSWD_PATH"
 chmod 600 "$PASSWD_PATH"
 
 # Remove old vnc locks
-vncserver -kill "$DISPLAY" &> "$START_DIR"/vnc_startup.log || rm -rf /tmp/.X*-lock /tmp/.X11-unix &> "$START_DIR"/vnc_startup.lo
+USER=zoomrec vncserver -kill "$DISPLAY" &> "$START_DIR"/vnc_startup.log || rm -rf /tmp/.X*-lock /tmp/.X11-unix &> "$START_DIR"/vnc_startup.log
+
+echo -e "\nDISPLAY = $DISPLAY\nVNC_COL_DEPTH = $VNC_COL_DEPTH\nVNC_RESOLUTION = $VNC_RESOLUTION\nVNC_IP = $VNC_IP\nVNC_PORT = $VNC_PORT"
+USER=zoomrec vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" &> "$START_DIR"/vnc_startup.log
+
+echo -e "\nConnect to $VNC_IP:$VNC_PORT"
 
 # Start xfce4
-${START_DIR}/xfce.sh &> "$START_DIR"/xfce.log
+"$HOME"/xfce.sh &> "$START_DIR"/xfce.log
 
 # Cleanup to ensure pulseaudio is stateless
 rm -rf /var/run/pulse /var/lib/pulse /home/zoomrec/.config/pulse
