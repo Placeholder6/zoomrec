c9effdbd33908234672c105706782d0db4e15e6a
Placeholder6 <86405576+Placeholder6@users.noreply.github.com>
2022-04-09 22:12:50 +0600

    Update entrypoint.sh

--- res/entrypoint.sh
+++ res/entrypoint.sh
@@ -1,33 +1,3 @@
-#!/bin/bash
-set -e
-
-cleanup () {
-    kill -s SIGTERM $!
-    exit 0
-}
-trap cleanup SIGINT SIGTERM
-
-VNC_IP=$(hostname -i)
-
-# Change vnc password
-mkdir -p "$HOME/.vnc"
-PASSWD_PATH="$HOME/.vnc/passwd"
-
-if [[ -f $PASSWD_PATH ]]; then
-    rm -f "$PASSWD_PATH"
-fi
-
-echo "$VNC_PW" | vncpasswd -f >> "$PASSWD_PATH"
-chmod 600 "$PASSWD_PATH"
-
-# Remove old vnc locks
-vncserver -kill "$DISPLAY" &> "$START_DIR"/vnc_startup.log || rm -rf /tmp/.X*-lock /tmp/.X11-unix &> "$START_DIR"/vnc_startup.log
-
-echo -e "\nDISPLAY = $DISPLAY\nVNC_COL_DEPTH = $VNC_COL_DEPTH\nVNC_RESOLUTION = $VNC_RESOLUTION\nVNC_IP = $VNC_IP\nVNC_PORT = $VNC_PORT"
-vncserver "$DISPLAY" -depth "$VNC_COL_DEPTH" -geometry "$VNC_RESOLUTION" &> "$START_DIR"/vnc_startup.log
-
-echo -e "\nConnect to $VNC_IP:$VNC_PORT"
-
 # Start xfce4
 ${START_DIR}/xfce.sh &> "$START_DIR"/xfce.log
 
